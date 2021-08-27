8350a31c717b74738dd9372a55732f0fbcd53047
Alfonso Andrés López Molina <osnoser_12@hotmail.com>
2021-08-27 13:24:56 -0500

    feat: connect angular app and rest api with net core

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+type WeatherForecast = {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary?: string;
+};
 
 @Component({
   selector: 'nx-angular-net-core-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-app';
+
+  weatherForecast$!: Observable<WeatherForecast[]>;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.weatherForecast$ = this.http.get<WeatherForecast[]>(
+      'https://localhost:5001/WeatherForecast'
+    );
+  }
 }
--- apps/netcore-api/Startup.cs
+++ apps/netcore-api/Startup.cs
@@ -26,6 +26,14 @@ namespace NxAngularNetCore.NetcoreApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("*");
+                    });
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -47,6 +55,8 @@ namespace NxAngularNetCore.NetcoreApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
